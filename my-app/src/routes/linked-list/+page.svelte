1da4d7456800c8181561d75d279bccceff9fe928
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-09 16:19:36 +0800

    added input field to append nodes to linked list

--- my-app/src/routes/linked-list/+page.svelte
+++ my-app/src/routes/linked-list/+page.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
     import LinkedList from '$lib/data-structures/linked-list'
 
-    let appendValue: string
     // example code to test
     const linkedList = new LinkedList()
-    linkedList.append(0)
-    linkedList.append(2)
     linkedList.prepend(4)
-    $: displayLinkedList = linkedList.toString()
+    linkedList.append(2)
+    let displayLinkedList = linkedList.toString()
+
+    // handle appending
+    function handleAppend(e: any) {
+        const formData = new FormData(e.target)
+        linkedList.append(formData.get('newNode') as unknown as number)
+        displayLinkedList = linkedList.toString()
+    }
 </script>
 
-<div class="flex flex-col size-[100vh] justify-center items-center">
+<div class="flex flex-col h-[100vh] w-full justify-center items-center">
+    <form on:submit={handleAppend}>
+        <input type="numeric" class="input px-3" id="newNode" required name="newNode">
+        <button type="submit">append</button>
+    </form>
+    <button on:click={() => console.log(linkedList.toString())}>log</button>
     <h1 class="h1 text-center">
         {displayLinkedList}
     </h1>
