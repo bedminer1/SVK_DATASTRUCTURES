ec71408580da6e4452a6f244b66606e32fc3c170
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-09 11:44:01 +0800

    constructor, append and prepend methods for LinkedList class

--- my-app/src/lib/data-structures/linked-list.ts
+++ my-app/src/lib/data-structures/linked-list.ts
@@ -1,8 +1,37 @@
-export default class Node {
+export class Node {
     value: number
-    nextNode: Node | null
+    next: Node | null
     constructor(value: number) {
         this.value = value || 0
-        this.nextNode = null
+        this.next = null
+    }
+}
+
+export class LinkedList {
+    listHead: Node | null
+    constructor() {
+        this.listHead = null
+    }
+
+    prepend(value: number) {
+        // temp to keep track of old head
+        let temp = null
+        if (this.listHead != null) 
+            temp = this.listHead
+        this.listHead = new Node(value)
+        this.listHead.next = temp
+    }
+
+    append(value: number) {
+        if (this.listHead === null)
+            this.prepend(value)
+        else {
+            let temp = this.listHead
+
+            // iterate till the end of the list
+            while (temp.next) 
+                temp = temp.next
+            temp.next = new Node(value)
+        }
     }
 }
