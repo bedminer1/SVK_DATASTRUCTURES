378b77d55ef44860ee4c88db22d25035db7ca277
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-10 01:10:46 +0800

    simple add and display for hashMap

--- my-app/src/lib/data-structures/hashmap.ts
+++ my-app/src/lib/data-structures/hashmap.ts
@@ -1,203 +1,189 @@
 class HashNode {
-    key: string
-    value: string | null
-    next: HashNode | null
+  key: string;
+  value: string;
+  next: HashNode | null;
 
-    constructor(key: string, value = null, next = null) {
-        this.key = key
-        this.value = value
-        this.next = next
-    }
+  constructor(key: string, value: string, next = null) {
+    this.key = key;
+    this.value = value;
+    this.next = next;
+  }
 }
 
 export default class HashMap {
-    bucketsArray: any[]
-    loadFactor: number
-    capacity: number
-    occupied: number
+  bucketsArray: any[];
+  loadFactor: number;
+  capacity: number;
+  occupied: number;
 
-    constructor() {
-        this.bucketsArray = new Array(16).fill(null)
-        this.loadFactor = 0.75
-        this.capacity = this.bucketsArray.length
-        this.occupied = 0
+  constructor() {
+    this.bucketsArray = new Array(16).fill(null);
+    this.loadFactor = 0.75;
+    this.capacity = this.bucketsArray.length;
+    this.occupied = 0;
+  }
+
+  hash(key: string): number {
+    let hashKey = 0;
+    const prime = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashKey += key.charCodeAt(i) * prime;
     }
+    return hashKey % this.capacity;
+  }
 
-    hash(key: string): number {
-        let hashKey = 0
-        const prime = 31
-        for (let i = 0; i < key.length; i++) {
-            hashKey += key.charCodeAt(i) * prime
+  resize() {
+    const oldArray = this.bucketsArray;
+    this.capacity *= 2;
+    this.bucketsArray = new Array(this.capacity).fill(null);
+    this.occupied = 0;
+
+    oldArray.forEach((bucket) => {
+      let curr = bucket;
+      while (curr) {
+        this.add(curr.key, curr.value);
+        curr = curr.next;
+      }
+    });
+  }
+
+  has(key: string) {
+    const bucket = this.hash(key);
+    let curr = this.bucketsArray[bucket];
+
+    // iterate thru LL to check
+    while (curr) {
+      if (curr.key === key) {
+        return true;
+      }
+      curr = curr.next;
+    }
+    return false;
+  }
+
+  add(key: string, value: string) {
+    // double bucketArr size if load exceeds 0.75
+    if (this.occupied / this.capacity >= this.loadFactor) {
+      this.resize();
+    }
+    const bucket = this.hash(key);
+    if (!this.has(key)) {
+      // if bucket doesn't contain key, add it to the bucket
+      const newNode = new HashNode(key, value);
+      if (this.bucketsArray[bucket] === null) {
+        this.occupied += 1;
+        this.bucketsArray[bucket] = newNode;
+      } else {
+        let curr = this.bucketsArray[bucket];
+        while (curr.next) {
+          curr = curr.next;
         }
-        return hashKey % this.capacity
+        curr.next = newNode;
+      }
+    } else {
+      // if it exists, update it
+      let curr = this.bucketsArray[bucket];
+      while (curr && curr.key !== key) curr = curr.next;
+      if (curr) curr.value = value;
     }
+  }
 
-    resize() {
-        const oldArray = this.bucketsArray
-        this.capacity *= 2
-        this.bucketsArray = new Array(this.capacity).fill(null)
-        this.occupied = 0
+  get(key: string): string {
+    const bucket = this.hash(key);
+    let curr = this.bucketsArray[bucket];
+    while (curr && curr.key !== key) curr = curr.next;
+    if (!curr) return "Key not in HashMap :(";
+    else return curr.value;
+  }
 
-        oldArray.forEach((bucket) => {
-            let curr = bucket
-            while (curr) {
-                this.add(curr.key, curr.value)
-                curr = curr.next
-            }
-        })
+  remove(key: string) {
+    const bucket = this.hash(key);
+    let curr = this.bucketsArray[bucket];
+    let prev = null;
+
+    while (curr && curr.key !== key) {
+      prev = curr;
+      curr = curr.next;
     }
+    // key not found
+    if (!curr) return;
 
-    has(key: string) {
-        const bucket = this.hash(key)
-        let curr = this.bucketsArray[bucket]
+    if (!prev && !curr.next) {
+      // if node is sole node in bucket
+      this.occupied -= 1;
+      this.bucketsArray[bucket] = null;
+    } else if (!prev) {
+      // if node is listHead
+      this.bucketsArray[bucket] = curr.next;
+    } else {
+      // deletion in LL
+      prev.next = curr.next;
+    }
+  }
 
-        // iterate thru LL to check
-        while (curr) {
-            if (curr.key === key) {
-                return true
-            }
-            curr = curr.next
+  length() {
+    let count = 0;
+    this.bucketsArray.forEach((bucket) => {
+      let curr = bucket;
+      if (bucket) {
+        count++;
+        while (curr.next) {
+          count++;
+          curr = curr.next;
         }
-        return false
-    }
+      }
+    });
+    return count;
+  }
 
-    add(key: string, value: any) {
-        // double bucketArr size if load exceeds 0.75
-        if (this.occupied / this.capacity >= this.loadFactor) {
-            this.resize()
+  clear() {
+    this.bucketsArray = new Array(16).fill(null);
+    this.occupied = 0;
+    this.capacity = this.bucketsArray.length;
+  }
+
+  keys() {
+    const arrayOfKeys: string[] = [];
+    this.bucketsArray.forEach((bucket) => {
+      let curr = bucket;
+      if (bucket) {
+        arrayOfKeys.push(curr.key);
+        while (curr.next) {
+          curr = curr.next;
+          arrayOfKeys.push(curr.key);
         }
-        const bucket = this.hash(key)
-        if (!this.has(key)) {
-            // if bucket doesn't contain key, add it to the bucket
-            const newNode = new HashNode(key, value)
-            if (this.bucketsArray[bucket] === null) {
-                this.occupied += 1
-                this.bucketsArray[bucket] = newNode
-            } 
-            
-            else {
-                let curr = this.bucketsArray[bucket]
-                while (curr.next) {
-                    curr = curr.next
-                }
-                curr.next = newNode
-            }
+      }
+    });
+    return arrayOfKeys;
+  }
+
+  values() {
+    const arrayOfValues: string[] = [];
+    this.bucketsArray.forEach((bucket) => {
+      let curr = bucket;
+      if (bucket) {
+        arrayOfValues.push(curr.value);
+        while (curr.next) {
+          curr = curr.next;
+          arrayOfValues.push(curr.value);
         }
-        else {
-            // if it exists, update it
-            let curr = this.bucketsArray[bucket]
-            while (curr && curr.key !== key) 
-                curr = curr.next
-            if (curr)
-                curr.value = value
+      }
+    });
+    return arrayOfValues;
+  }
+
+  entries() {
+    const arrayOfEntries: string[][] = [];
+    this.bucketsArray.forEach((bucket) => {
+      let curr = bucket;
+      if (bucket) {
+        arrayOfEntries.push([curr.key, curr.value]);
+        while (curr.next) {
+          curr = curr.next;
+          arrayOfEntries.push([curr.key, curr.value]);
         }
-    }
-
-    get(key: string): string {
-        const bucket = this.hash(key)
-        let curr = this.bucketsArray[bucket]
-        while (curr && curr.key !== key) 
-            curr = curr.next
-        if (!curr)
-            return 'Key not in HashMap :('
-        else
-            return curr.value
-    }
-
-    remove(key: string) {
-        const bucket = this.hash(key)
-        let curr = this.bucketsArray[bucket]
-        let prev = null
-
-        while (curr && curr.key !== key) {
-            prev = curr
-            curr = curr.next
-        }
-        // key not found
-        if (!curr)
-            return
-
-        if (!prev && !curr.next) {
-            // if node is sole node in bucket
-            this.occupied -= 1
-            this.bucketsArray[bucket] = null
-        } 
-        else if (!prev) {
-            // if node is listHead
-            this.bucketsArray[bucket] = curr.next
-        } 
-        else {
-            // deletion in LL
-            prev.next = curr.next
-        }
-    }
-
-    length() {
-        let count = 0
-        this.bucketsArray.forEach((bucket) => {
-            let curr = bucket
-            if (bucket) {
-                count++
-                while (curr.next) {
-                    count++
-                    curr = curr.next
-                }
-            }
-        })
-        return count
-    }
-
-    clear() {
-        this.bucketsArray = new Array(16).fill(null)
-        this.occupied = 0
-        this.capacity = this.bucketsArray.length
-    }
-
-    keys() {
-        const arrayOfKeys: string[] = []
-        this.bucketsArray.forEach((bucket) => {
-            let curr = bucket
-            if (bucket) {
-                arrayOfKeys.push(curr.key)
-                while (curr.next) {
-                    curr = curr.next
-                    arrayOfKeys.push(curr.key)
-                }
-                
-            }
-        })
-        return arrayOfKeys
-    }
-
-    values() {
-        const arrayOfValues: string[] = []
-        this.bucketsArray.forEach((bucket) => {
-            let curr = bucket
-            if (bucket) {
-                arrayOfValues.push(curr.values)
-                while (curr.next) {
-                    curr = curr.next
-                    arrayOfValues.push(curr.values)
-                }
-                
-            }
-        })
-        return arrayOfValues
-    }
-
-    entries() {
-        const arrayOfEntries: string[][] = []
-        this.bucketsArray.forEach((bucket) => {
-            let curr = bucket
-            if (bucket) {
-                arrayOfEntries.push([curr.key, curr.values])
-                while (curr.next) {
-                    curr = curr.next
-                    arrayOfEntries.push([curr.key, curr.values])
-                }
-                
-            }
-        })
-        return arrayOfEntries
-    }
+      }
+    });
+    return arrayOfEntries;
+  }
 }
--- my-app/src/routes/hash-map/+page.svelte
+++ my-app/src/routes/hash-map/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
     import HashMap from '$lib/data-structures/hashmap'
 
+    // example code to test
     const hashMap = new HashMap()
-    hashMap.add('hi', 'mom')
+    let entries: string
+
+    function handleAdd(e: any) {
+        const formData = new FormData(e.target)
+        const key = formData.get('key') as string
+        const value = formData.get('value') as string
+        hashMap.add(key, value)
+        entries = JSON.stringify(hashMap.entries())
+    }
 
 </script>
+
+<div class="flex flex-col h-[100vh] gap-5 w-full justify-center items-center">
+    <!-- <button on:click={() => console.log(linkedList.toString())}>log</button> -->
+    <h1 class="h1 text-center">
+        {entries}
+    </h1>
+    <form on:submit={handleAdd} class="card p-3 rounded-sm flex justify-center gap-4 items-center flex-col">
+        <input type="text" class="input px-3 rounded-sm" id="key" required name="key" placeholder="key">
+        <input type="text" class="input px-3 rounded-sm" id="value" required name="value" placeholder="value">
+        <button type="submit" class="rounded-md w-20 variant-filled h-8">add</button>
+    </form>
+</div>
